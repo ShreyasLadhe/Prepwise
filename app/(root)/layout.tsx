c61a7c2cf0b51e0294158497b9c2dfdd2b7016b3
Shreyas Ladhe <shreyasladhe.23@gmail.com>
2025-03-24 18:46:40 +0530

    dummy homepage finished

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,19 @@
 import {ReactNode} from 'react'
+import Link from "next/link";
+import Image from "next/image";
 
-const RootLayout = ({children}: {children : ReactNode}) => {
+const RootLayout = ({ children }: { children: ReactNode }) => {
     return (
-        <div>{children}</div>
-    )
-}
-export default RootLayout
+        <div className="root-layout">
+            <nav className="flex items-center gap-2">
+                <Link href="/" className="flex items-center gap-2">
+                    <Image src="/logo.svg" alt="Logo" width={38} height={32} />
+                </Link>
+                <h2 className="text-primary-100">PrepWise</h2>
+            </nav>
+            {children}
+        </div>
+    );
+};
+export default RootLayout;
+
